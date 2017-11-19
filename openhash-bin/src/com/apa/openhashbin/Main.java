94443bd19f421f59b2fe48cb8505dbfa941023bc
Lucas Cota <lucasbrunocota@live.com>
2017-11-19 15:20:34 -0200

    Hash de endereçamento aberto implementada.

--- openhash-bin/src/com/apa/openhashbin/Main.java
+++ openhash-bin/src/com/apa/openhashbin/Main.java
@@ -3,7 +3,7 @@
  * Author: Lucas Cota
  * Description: openhash-bin Main.
  * Date: 2017-11-17
- * Modified: 2017-11-17
+ * Modified: 2017-11-19
  */
 
 package com.apa.openhashbin;
@@ -11,6 +11,7 @@ package com.apa.openhashbin;
 import java.util.ArrayList;
 
 import com.apa.openhashbin.Helpers.Randoman;
+import com.apa.openhashbin.Stream.OpenHashMap;
 import com.apa.openhashbin.Stream.BinarySearchTree;
 
 public class Main
@@ -20,23 +21,54 @@ public class Main
 
 	public static void main(String[] args)
 	{
+		OpenHashMap openHashMap = new OpenHashMap(VALUE_RANGE);
 		BinarySearchTree binarySearchTree = new BinarySearchTree();
 		
 		ArrayList<Integer> rangedArray = Randoman.RangedArray(VALUE_RANGE);
 		ArrayList<Integer> rangedTestArray = Randoman.RandomicArray(ARRAY_SIZE_TEST, VALUE_RANGE);
 		
-		for (int currentValue : rangedArray)
+		for (int i = 0; i < rangedArray.size(); i++)
 		{
+			int currentValue = rangedArray.get(i);
+			
 			binarySearchTree.Insert(currentValue);
+			
+			// Randomic key "name" of hash in a preset VALUE_RANGE.
+			openHashMap.Insert(Randoman.Generate(VALUE_RANGE), currentValue);
 		}
 		
-		binarySearchTree.ShowResultHeader(VALUE_RANGE, ARRAY_SIZE_TEST);
+		/* Possible BinarySearchTree commands
+		 * binarySearchTree.Insert(int nodeValue): Inserts a new node on tree.
+		 * binarySearchTree.Delete(int nodeToDelete): Delete an exist node on tree.
+		 * binarySearchTree.Find(int nodeValue): Find an exist node on tree.
+		 * binarySearchTree.DisplayHeader(int VALUE_RANGE, int ARRAY_SIZE_TEST): Show a simple output header for results.
+		 * binarySearchTree.Display(int currentValue, boolean searchResult): Show the detailed results of the test.
+		 */
+		
+		binarySearchTree.DisplayHeader(VALUE_RANGE, ARRAY_SIZE_TEST);
 
 		for (int currentValue : rangedTestArray)
 		{
 			boolean searchResult = binarySearchTree.Find(currentValue);
 			
-			binarySearchTree.ShowResult(currentValue, searchResult);
+			binarySearchTree.Display(currentValue, searchResult);
+		}
+		
+		/* Possible BinarySearchTree commands
+		 * openHashMap.Insert(int key, int value): Inserts a new value on hash table.
+		 * openHashMap.Delete(int key): Delete an exist value on hash table.
+		 * openHashMap.Find(int key): Find an exist value on hash table.
+		 * openHashMap.DisplayHeader(int VALUE_RANGE, int ARRAY_SIZE_TEST): Show a simple output header for results.
+		 * openHashMap.Display(int currentValue, boolean searchResult): Show the detailed results of the test.
+		 */
+		
+		openHashMap.DisplayHeader(VALUE_RANGE, ARRAY_SIZE_TEST);
+
+		for (int currentValue : rangedTestArray)
+		{
+			int searchResult = openHashMap.Find(currentValue);
+			
+			openHashMap.Display(currentValue, searchResult);
 		}
 	}
 }
---
+++ openhash-bin/src/com/apa/openhashbin/Stream/OpenHashMap.java
@@ -0,0 +1,233 @@
+/*
+ * OpenHashMap.java
+ * Author: Lucas Cota
+ * Description: Open HashMap common operations.
+ * Reference: <http://www.algolist.net/Data_structures/Hash_table/Open_addressing>
+ * Date: 2017-11-19
+ * Modified: 2017-11-19
+ */
+
+package com.apa.openhashbin.Stream;
+
+import com.apa.openhashbin.Helpers.TimeMeasure;
+
+public class OpenHashMap
+{
+	// HashTable
+	private int tableSize;
+	private HashEntry[] hashTable;
+	
+	// Counters
+	private float searchTime;
+	private int searchComparisons;
+
+
+
+
+	// Default Constructor
+	public OpenHashMap(int tableSize)
+	{
+		this.tableSize = tableSize;
+
+		// Cria uma nova tabela hash de acordo com o tamanho passado pelo construtor.
+		hashTable = new HashEntry[this.tableSize];
+		
+		// Seta todas as posições inicialmente como nulo.
+		for (int i = 0; i < this.tableSize; i++)
+		{
+			hashTable[i] = null;
+		}
+	}
+
+
+
+
+	/*
+	 * Methods
+	 */
+
+	public void Insert(int key, int value)
+	{
+		// Fórmula padrão do cálculo da hash de acordo com a chave.
+		int hashFormula = (key % tableSize);
+		
+		// Cria uma nova entrada na tablea hash de acordo com a fórmula padrão.
+		hashTable[hashFormula] = new HashEntry(key, value);
+	}
+	
+	public Boolean Delete(int key)
+	{
+		// Fórmula padrão usada para encontrar o valor da hash de acordo com a chave.
+		int hashFormula = (key % tableSize);
+		
+		// Se encontrar, seta a entrada como removida, onde a chave e o valor são setados como -1.
+		if (hashTable[hashFormula] != null)
+		{
+			hashTable[hashFormula] = HashRemovedEntry.getUniqueRemovedEntry();
+			
+			return true;
+		}
+		
+		return false;
+	}
+		
+	public int Find(int key)
+	{
+		// Time measure start time
+		long startTime = TimeMeasure.NanoTime();
+
+		// Fórmula padrão usada para encontrar o valor da hash de acordo com a chave.
+		int hashFormula = (key % tableSize);
+
+		// Se não encontrar, retorna -1.
+		if (hashTable[hashFormula] == null)
+		{
+			searchComparisons++;
+
+			// Time measure final time
+			searchTime = TimeMeasure.MeasureNanoToMilis(startTime);
+
+			return -1;
+		}
+
+		// Se encontrar, retorna o valor da hash de acordo com a key usada na fórmula.
+		else
+		{
+			searchComparisons++;
+
+			// Time measure final time
+			searchTime = TimeMeasure.MeasureNanoToMilis(startTime);
+
+			return hashTable[hashFormula].getValue();
+		}
+	}
+	
+	public void DisplayHeader(int VALUE_RANGE, int ARRAY_SIZE_TEST)
+	{
+		System.out.println
+		(
+			String.format
+			(
+				"%sOpenHashMap(ARRAY_SIZE_TEST: %d, VALUE_RANGE: %d)%s",
+				System.lineSeparator(),
+				VALUE_RANGE,
+				ARRAY_SIZE_TEST,
+				System.lineSeparator()
+			)
+		);
+	}
+	
+	public void Display(int currentValue, int searchResult)
+	{
+		String searchValue;
+
+		if (searchResult == -1)
+		{
+			searchValue = "Not Found";
+		}
+		else
+		{
+			searchValue = String.valueOf(searchResult);
+		}
+
+		System.out.println
+		(
+			String.format
+			(
+				"FindKey: %4d | FindTime: %f ms | FindComparisons: %4d | ValueOfKey: %s",
+				currentValue,
+				getSearchTime(),
+				getSearchComparisons(),
+				searchValue
+			)
+		);
+	}
+
+
+
+
+	/*
+	 * Getters/Setters
+	 */
+
+	public float getSearchTime()
+	{
+		float currentSearchTime = searchTime;
+		
+		searchTime = 0;
+		
+		return currentSearchTime;
+	}
+
+	public int getSearchComparisons()
+	{
+		int currentSearchComparisons = searchComparisons;
+		
+		searchComparisons = 0;
+
+		return currentSearchComparisons;
+	}
+}
+
+
+
+
+// HashEntry Structure
+class HashEntry
+{
+	private int key;
+	private int value;
+	
+	public HashEntry(int key, int value)
+	{
+		this.key = key;
+		this.value = value;
+	}
+
+	public int getKey()
+	{
+		return key;
+	}
+
+	public void setKey(int key)
+	{
+		this.key = key;
+	}
+
+	public int getValue()
+	{
+		return value;
+	}
+
+	public void setValue(int value)
+	{
+		this.value = value;
+	}
+}
+
+
+
+
+/*
+ * HashTable Removed Entries
+ * A entrada removida da HashTable é setada para -1 em sua chave e valor.
+ */
+class HashRemovedEntry extends HashEntry
+{
+	private static HashRemovedEntry hashEntry = null;
+
+	private HashRemovedEntry()
+	{
+		super(-1, -1);
+	}
+	
+	public static HashRemovedEntry getUniqueRemovedEntry()
+	{
+		if (hashEntry == null)
+		{
+			hashEntry = new HashRemovedEntry();
+		}
+
+		return hashEntry;
+	}
+}
